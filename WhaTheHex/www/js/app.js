b691a7d75d74dd6823ac33724e71d38862e07740
bcorber <bcorber@gmail.com>
2015-09-24 12:30:54 -0700

    Added on device ready function of opening camera and on page load for get photo from gallery

--- WhaTheHex/www/js/app.js
+++ WhaTheHex/www/js/app.js
@@ -19,31 +19,77 @@
     
     //Live camera view controller
     
-  module.controller('LiveController', function($scope, $data) 
-    {
-
-        //where the magic happens... on device ready GetCamera
-        var mousePos = getMousePos(canvas, evt);
-        var color = undefined;
+  module.controller('LiveController', function($scope, $data) {
       
-      if(mouseDown && mousePos !== null && mousePos.x > padding && mousePos.x < padding + imageObj.width && mousePos.y > padding && mousePos.y < padding + imageObj.height) {
-
-            // color picker image is 256x256 and is offset by 10px
-            // from top and bottom
-            var imageData = context.getImageData(padding, padding, imageObj.width, imageObj.width);
-            var data = imageData.data;
-            var x = mousePos.x - padding;
-            var y = mousePos.y - padding;
-            var red = data[((imageObj.width * y) + x) * 4];
-            var green = data[((imageObj.width * y) + x) * 4 + 1];
-            var blue = data[((imageObj.width * y) + x) * 4 + 2];
-//            var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
-//            drawColorSquare(canvas, color, imageObj);
-          }
-//        }, false
-
       
+      
+//        var setCanvasFromImage = function(_data) {
+//        var img = new Image,
+//        canvas = document.createElement("canvas"),
+//        ctx = canvas.getContext("2d"),
+//        src = "data:image/jpeg;base64," + _data; // insert image url here
+//
+//        img.crossOrigin = "Anonymous";
+//
+//        img.onload = function() {
+//            canvas.width = img.width;
+//            canvas.height = img.height;
+//            ctx.drawImage( img, 0, 0 );
+//            localStorage.setItem( "savedImageData", canvas.toDataURL("image/jpeg") );
+//
+//            img.src = src;
+//            // make sure the load event fires for cached images too
+//            if ( img.complete || img.complete === undefined ) {
+//                img.src = "data:image/jpeg;base64," + _data;
+//                img.src = src;
+//            }
+//        }
+//    }
+//        
+//        var canvas = document.getElementById('canvas');
+//        var context = canvas.getContext('2d');
+//        canvas.width = window.innerWidth;
+//        canvas.height = window.innerHeight;
     });
+      
+//    var canvas = document.getElementById('canvas_picker').getContext('2d');
+//
+//    // create an image object and get it’s source
+//    var img = new Image();
+//    img.src = "data:image/jpeg;base64," + _data;
+//
+//    // copy the image to the canvas
+//    $(img).load(function(){
+//      canvas.drawImage(img,0,0);
+//    });
+//          
+//      
+//
+//	// http://www.javascripter.net/faq/rgbtohex.htm
+//	function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
+//	function toHex(n) {
+//	  n = parseInt(n,10);
+//	  if (isNaN(n)) return "00";
+//	  n = Math.max(0,Math.min(n,255));
+//	  return "0123456789ABCDEF".charAt((n-n%16)/16)  + "0123456789ABCDEF".charAt(n%16);
+//	}
+//	$('#canvas_picker').click(function(event){
+//	  // getting user coordinates
+//	  var x = event.pageX - this.offsetLeft;
+//	  var y = event.pageY - this.offsetTop;
+//	  // getting image data and RGB values
+//	  var img_data = canvas.getImageData(x, y, 1, 1).data;
+//	  var R = img_data[0];
+//	  var G = img_data[1];
+//	  var B = img_data[2];  var rgb = R + ',' + G + ',' + B;
+//	  // convert RGB to HEX
+//	  var hex = rgbToHex(R,G,B);
+//	  // making the color the value of the input
+//	  $('#rgb input').val(rgb);
+//	  $('#hex input').val('#' + hex);
+//	});
+ 
+    
     
     //Device's photo gallery controller
     
