44d484648eecf462edcac92133640e16dab800a1
JesseScott <jessecolinscott@gmail.com>
2015-10-03 00:48:59 -0700

    killed data. killed onclick from menu in favour of using onDeviceReady. passing photo picker to canvas.

--- WhaTheHex/www/js/app.js
+++ WhaTheHex/www/js/app.js
@@ -5,9 +5,9 @@
 
   //Live camera view controller
 
-  module.controller('LiveController', function($scope, $data) {
+  module.controller('LiveController', function($scope) {
 
-      $scope.picked = '00FFFF';
+      $scope.hex = '00FFFF';
       var pictureSource;
       var destinationType;
 
@@ -18,7 +18,6 @@
         pictureSource = navigator.camera.PictureSourceType;
         destinationType = navigator.camera.DestinationType;
         capturePhoto();
-
       }
 
       function capturePhoto() {
@@ -52,7 +51,7 @@
         var padding = 10;
         var mouseDown = false;
 
-        var canvas = document.getElementById('canvas');
+        var canvas = document.getElementById('live-canvas');
         var context = canvas.getContext('2d');
         context.strokeStyle = '#444';
         context.lineWidth = 2;
@@ -88,8 +87,8 @@
       }
 
       function updateColor(R, G, B) {
-        $scope.picked = rgbToHex(R, G, B);
-        //console.log($scope.picked);
+        $scope.hex = rgbToHex(R, G, B);
+        //console.log($scope.hex);
         $scope.$apply();
       }
 
@@ -120,33 +119,28 @@
 
   //Device's photo gallery controller
 
-  module.controller('GalleryController', function($scope, $data) {
-    
+  module.controller('GalleryController', function($scope) {
+
       var pictureSource;
       var destinationType;
 
       document.addEventListener("deviceready", onDeviceReady, true);
 
-      function onPageload() {
+      function onDeviceReady() {
+        console.log('onDeviceReady');
         pictureSource = navigator.camera.PictureSourceType;
         destinationType = navigator.camera.DestinationType;
         getPhoto(pictureSource.PHOTOLIBRARY);
       }
 
-      function onPhotoDataSuccess(imageData) {
-        var largeImage = document.getElementById('largeImage');
-        largeImage.style.display = 'block';
-        largeImage.src = "data:image/jpeg;base64," + imageData;
-      }
-
-      function onPhotoURISuccess(imageURI) {
-        var largeImage = document.getElementById('largeImage');
-        largeImage.style.display = 'block';
-        largeImage.src = imageURI;
+      function onSuccess(imageURI) {
+        console.log('onSuccess');
+        setupImage(imageURI);
       }
 
       function getPhoto(source) {
-        navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+        console.log('getPhoto');
+        navigator.camera.getPicture(onSuccess, onFail, {
           quality: 50,
           destinationType: destinationType.FILE_URI,
           sourceType: source
@@ -157,17 +151,96 @@
         alert('Failed because: ' + message);
       }
 
+      function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+      }
+
+      function init(imageObj) {
+        console.log('init');
+
+        var padding = 10;
+        var mouseDown = false;
+
+        var canvas = document.getElementById('gallery-canvas');
+        var context = canvas.getContext('2d');
+        context.strokeStyle = '#444';
+        context.lineWidth = 2;
+        context.canvas.width  = window.innerWidth - padding;
+        context.canvas.height = window.innerWidth - padding;
+
+        canvas.addEventListener('mousedown', function() {
+          mouseDown = true;
+        }, false);
+
+        canvas.addEventListener('mouseup', function(event) {
+          console.log('mouseup');
+
+          //mouseDown = false;
+          var mousePos = getMousePos(canvas, event);
+          var color = undefined;
+          if (mouseDown && mousePos !== null && mousePos.x > padding && mousePos.x < padding + imageObj.width && mousePos.y > padding && mousePos.y < padding + imageObj.height) {
+            var imageData = context.getImageData(padding, padding, imageObj.width, imageObj.width);
+            var data = imageData.data;
+            var x = mousePos.x - padding;
+            var y = mousePos.y - padding;
+            var red = data[((imageObj.width * y) + x) * 4];
+            var green = data[((imageObj.width * y) + x) * 4 + 1];
+            var blue = data[((imageObj.width * y) + x) * 4 + 2];
+            var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+            console.log('picked color is ' + color);
+            updateColor(red, green, blue);
+          }
+
+        }, false);
+
+        context.drawImage(imageObj, padding, padding);
+      }
+
+      function updateColor(R, G, B) {
+        $scope.hex = rgbToHex(R, G, B);
+        console.log($scope.hex);
+        $scope.$apply();
+      }
+
+      function rgbToHex(R, G, B) {
+        return toHex(R) + toHex(G) + toHex(B)
+      }
+
+      function toHex(n) {
+        n = parseInt(n, 10);
+        if (isNaN(n)) {
+          return "00";
+        }
+        n = Math.max(0, Math.min(n, 255));
+        return "0123456789ABCDEF".charAt((n - n % 16) / 16) + "0123456789ABCDEF".charAt(n % 16);
+      }
+
+      function setupImage(_data) {
+        console.log('setupImage');
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+          init(this);
+        };
+        imageObj.src = _data;
+      }
+
+
   });
 
   // App's saved colour pallete controller
 
-  module.controller('PalletteController', function($scope, $data) {
+  module.controller('PalletteController', function($scope) {
     //data pushed
   });
 
   //App's RGB slider controller
 
-  module.controller('RgbController', function($scope, $data) {
+  module.controller('RgbController', function($scope) {
 
     $scope.red = 0;
     $scope.green = 255;
@@ -202,34 +275,34 @@
 
   //App's Help page controller
 
-  module.controller('HelpController', function($scope, $data) {
+  module.controller('HelpController', function($scope) {
     //where the magic happens... N/A
   });
 
 
 
-  module.factory('$data', function() {
-    var data = {};
-
-    data.items = [{
-      title: 'Item 1 Title',
-      label: '4h',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    }, {
-      title: 'Another Item Title',
-      label: '6h',
-      desc: 'Ut enim ad minim veniam.'
-    }, {
-      title: 'Yet Another Item Title',
-      label: '1day ago',
-      desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-    }, {
-      title: 'Yet Another Item Title',
-      label: '1day ago',
-      desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-    }];
-
-    return data;
-  });
+  // module.factory('$data', function() {
+  //   var data = {};
+  //
+  //   data.items = [{
+  //     title: 'Item 1 Title',
+  //     label: '4h',
+  //     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  //   }, {
+  //     title: 'Another Item Title',
+  //     label: '6h',
+  //     desc: 'Ut enim ad minim veniam.'
+  //   }, {
+  //     title: 'Yet Another Item Title',
+  //     label: '1day ago',
+  //     desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+  //   }, {
+  //     title: 'Yet Another Item Title',
+  //     label: '1day ago',
+  //     desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+  //   }];
+  //
+  //   return data;
+  // });
 
 })();
